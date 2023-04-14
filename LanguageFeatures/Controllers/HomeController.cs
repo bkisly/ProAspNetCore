fc3f20b81d275c053b3e4ff92d5cbe1f040c882b
bkisly <b.kisly@gmail.com>
2023-04-14 11:29:02 +0200

    Added controller, view and global usings

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Product[] products = Product.GetProducts();
+            return View(new string[] { products[0].Name });
+        }
+    }
+}
