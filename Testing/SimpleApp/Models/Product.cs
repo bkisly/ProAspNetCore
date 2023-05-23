14ff8f152bbd6d1c9c5958283f8f3672c7892588
bkisly <b.kisly@gmail.com>
2023-05-23 10:56:57 +0200

    Created basic application and empty project for testing

---
+++ Testing/SimpleApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal? Price { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            var kayak = new Product { Name = "Kayak", Price = 275M };
+            var lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+
+            return new[] { kayak, lifejacket };
+        }
+    }
+}
