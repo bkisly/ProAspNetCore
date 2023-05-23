a605419e91fba84059d7f116abebc30d4dde1f3f
bkisly <b.kisly@gmail.com>
2023-05-23 19:32:51 +0200

    Finished chapter 6

--- Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SimpleApp.Controllers;
 using SimpleApp.Models;
+using Moq;
 
 namespace SimpleApp.Tests
 {
@@ -21,17 +22,22 @@ namespace SimpleApp.Tests
                 new Product { Name = "P3", Price = 110M }
             };
 
-            IProductsDataSource data = new FakeProductsDataSource(testData);
-            var controller = new HomeController { ProductsDataSource = data };
+            var mock = new Mock<IProductsDataSource>();
+            mock.SetupGet(m => m.Products).Returns(testData);
+
+            //IProductsDataSource data = new FakeProductsDataSource(testData);
+            var controller = new HomeController { ProductsDataSource = mock.Object };
 
             // Act
             var model = (controller.Index() as ViewResult)?.ViewData.Model
                 as IEnumerable<Product>;
 
             // Assert
-            Assert.Equal(data.Products, model,
+            Assert.Equal(testData, model,
                 Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name
                                                   && p1?.Price == p2?.Price));
+
+            mock.VerifyGet(m => m.Products, Times.Once);
         }
     }
 }
