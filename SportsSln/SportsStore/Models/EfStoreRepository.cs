2cfbfc6afd2e83c6970f758adb99690654ac11ce
bkisly <b.kisly@gmail.com>
2023-05-24 11:57:39 +0200

    Chapter 7: Created model, controller, configured EF and added repository class

---
+++ SportsSln/SportsStore/Models/EfStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace SportsStore.Models
+{
+    public class EfStoreRepository : IStoreRepository
+    {
+        private readonly StoreDbContext _context;
+
+        public EfStoreRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Product> Products => _context.Products;
+    }
+}
--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,5 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(options =>
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]));
+
+builder.Services.AddScoped<IStoreRepository, EfStoreRepository>();
 
 var app = builder.Build();
 
