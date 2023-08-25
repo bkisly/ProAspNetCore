f4c83d7b47acf7978ed607d9b3ca952fc9a4590f
bkisly <b.kisly@gmail.com>
2023-08-26 01:47:45 +0200

    Chapter 7: Styled layout

--- SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -24,6 +24,11 @@ namespace SportsStore.Infrastructure
 
         public string? PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -40,6 +45,13 @@ namespace SportsStore.Infrastructure
                             ["href"] = urlHelper.Action(PageAction, new { productPage = i })
                         }
                     };
+
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage
+                            ? PageClassSelected : PageClassNormal);
+                    }
 
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
